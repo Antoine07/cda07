62ae16d72abad999e0b65d8056e0edd54f3ca856
Antoine07 <antoine.lucsko@gmail.com>
2021-09-13 12:52:55 +0200

    ttc sum

---
+++ JS_PHP/fondamentaux_Part1/Exercices/04_spread_ttc.js
@@ -0,0 +1,21 @@
+// 0.2 <=> .2
+function sumTTC(p1, p2, p3, tva = 0.2) {
+   
+    p1 = parseFloat(p1);
+    p2 = parseFloat(p2);
+    p3 = parseFloat(p3);
+    tva = parseFloat(tva);
+
+    // vérification du type des variables 
+    if(isNaN(p1) ||isNaN(p2) || isNaN(p3) || isNaN(tva) ){
+        console.error("L'une des valeurs n'est pas un nombre vérifier le type de vos prix");
+
+        return;
+    }
+
+    price = p1 + p2 + p3 ;
+  
+    return Math.floor(price * (1 + tva) * 100) / 100;
+  }
+  
+  console.log(sumTTC( ...[10,189, 780,]), (10+189+ 780)*1.2);
