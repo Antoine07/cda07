be6a58f98afdf32c328be87579c66977a59e900e
Antoine07 <antoine.lucsko@gmail.com>
2021-09-15 15:48:14 +0200

    async

--- JS_PHP/async/Exercices/01_callback_email.js
+++ JS_PHP/async/Exercices/01_callback_email.js
@@ -1,5 +1,12 @@
 const login = (email, password, callback ) => {
     setTimeout(() => {
-        // ...
+        callback(email, password);
     }, 1000);
 }
+
+console.log("Start");
+login('alan@aln.fr', '123', (email, password) => {
+    console.log(email, password);
+    // On peut faire des mise à jour du DOM par exemple ici
+});
+console.log("End");
