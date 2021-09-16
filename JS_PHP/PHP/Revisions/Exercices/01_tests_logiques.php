460aef8eeaee64bc49d0d8d9f0156cad4f394524
Antoine07 <antoine.lucsko@gmail.com>
2021-09-16 10:33:25 +0200

    exercices

---
+++ JS_PHP/PHP/Revisions/Exercices/01_tests_logiques.php
@@ -0,0 +1,3 @@
+<?php
+
+$exp1 = true;
---
+++ JS_PHP/PHP/Revisions/Exercices/app.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/01_tests_logiques.php';
+
+function al_dump($results)
+{
+    echo $results . PHP_EOL ; // retour à la ligne
+}
+
